3bcd8c58ebd40ce5b1e6422864bc5a63e66ebeba
Thiago Reis <thiagophoxx@gmail.com>
2012-02-16 23:35:19 -0200

    criação do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webstore::Application.config.secret_token = 'aa93e8b05c7098fbaee6a5addd4d358c3120bc337b85eecff8b74802ba9cea151e6a36d42cca735769ed9422ce02d98a624ab9b22be1082dee1e0169cc70601a'
